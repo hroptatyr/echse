d3fe2052dc92b9434be2f7daf3a85debd62ceee8
Sebastian Freundt <freundt@ga-group.nl>
2013-03-01 10:32:44 +0000

    minor, refactor filter tool so that a filter in turn is turned into a stream
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/echse-filter.c
+++ src/echse-filter.c
@@ -90,7 +90,7 @@ materialise(echs_event_t e)
 
 
 /* myself as filter */
-struct echse_clo_s {
+struct echsf_clo_s {
 	echs_fltdef_t fd;
 };
 
@@ -103,7 +103,7 @@ __identity(echs_event_t e, void *UNUSED(clo))
 static echs_event_t
 __filter(echs_event_t e, void *clo)
 {
-	struct echse_clo_s *x = clo;
+	struct echsf_clo_s *x = clo;
 
 	return echs_filter_next(x->fd.f, e);
 }
@@ -111,7 +111,7 @@ __filter(echs_event_t e, void *clo)
 echs_filter_t
 make_echs_filter(echs_instant_t from, ...)
 {
-	static struct echse_clo_s x;
+	static struct echsf_clo_s x;
 	va_list ap;
 	const char *fn;
 
@@ -128,11 +128,105 @@ make_echs_filter(echs_instant_t from, ...)
 void
 free_echs_filter(echs_filter_t f)
 {
-	struct echse_clo_s *clo = f.clo;
+	struct echsf_clo_s *clo = f.clo;
 
 	if (LIKELY(clo != NULL)) {
 		echs_close_fltdef(clo->fd);
 	}
+	return;
+}
+
+
+/* myself as stream */
+struct echss_clo_s {
+	unsigned int drain;
+	echs_filter_t filt;
+	/* stuff we ctor */
+	echs_stream_t strm;
+	echs_mod_t m;
+};
+
+static echs_event_t
+__stream(void *clo)
+{
+	struct echss_clo_s *x = clo;
+
+	switch (x->drain) {
+	case 0:
+		/* iterate! */
+		for (echs_event_t e;
+		     (e = echs_stream_next(x->strm), !__event_0_p(e));) {
+			if (!__event_0_p(e = echs_filter_next(x->filt, e))) {
+				return e;
+			}
+		}
+		x->drain = 1;
+	case 1:
+		/* drain */
+		for (echs_event_t e;
+		     (e = echs_filter_drain(x->filt), !__event_0_p(e));) {
+			return e;
+		}
+	default:
+		break;
+	}
+	return (echs_event_t){0};
+}
+
+echs_stream_t
+make_echs_stream(echs_instant_t inst, ...)
+{
+/* INST, FILTER, STRDEF, NSTRDEF */
+	typedef echs_stream_t(*make_stream_f)(echs_instant_t, ...);
+	static struct echss_clo_s clo;
+	va_list ap;
+	echs_filter_t f;
+	const char *const *in;
+	size_t nin;
+	echs_mod_f mf;
+
+	va_start(ap, inst);
+	f = va_arg(ap, echs_filter_t);
+	in = va_arg(ap, const char *const*);
+	nin = va_arg(ap, size_t);
+	va_end(ap);
+
+	if ((clo.m = echs_mod_open("echse")) == NULL) {
+		printf("NOPE\n");
+		goto out;
+	} else if ((mf = echs_mod_sym(clo.m, "make_echs_stream")) == NULL) {
+		printf("no m_e_s()\n");
+		goto out;
+	} else if ((clo.strm = ((make_stream_f)mf)(inst, in, nin)).f == NULL) {
+		printf("no stream\n");
+		goto out;
+	}
+	/* assign the filter */
+	clo.filt = f;
+
+	return (echs_stream_t){__stream, &clo};
+
+out:
+	return (echs_stream_t){NULL, &clo};
+}
+
+void
+free_echs_stream(echs_stream_t s)
+{
+	typedef void(*free_stream_f)(echs_stream_t);
+	struct echss_clo_s *clo = s.clo;
+	echs_mod_f f;
+
+	if (UNLIKELY(clo->m == NULL)) {
+		return;
+	} else if ((f = echs_mod_sym(clo->m, "free_echs_stream")) == NULL) {
+		printf("no f_e_s()\n");
+		goto clos_out;
+	}
+	((free_stream_f)f)(clo->strm);
+
+clos_out:
+	echs_mod_close(clo->m);
 	return;
 }
 
@@ -161,9 +255,8 @@ main(int argc, char *argv[])
 	struct echs_args_info argi[1];
 	echs_instant_t from;
 	echs_instant_t till;
-	echs_mod_t ex;
-	echs_stream_t s;
-	echs_filter_t this;
+	echs_filter_t thif;
+	echs_stream_t this;
 	int res = 0;
 
 	if (echs_parser(argc, argv, argi)) {
@@ -184,61 +277,28 @@ main(int argc, char *argv[])
 	}
 
 	if (argi->filter_given) {
-		this = make_echs_filter(from, argi->filter_arg);
+		thif = make_echs_filter(from, argi->filter_arg);
 	} else {
-		this = (echs_filter_t){__identity, NULL};
+		thif = (echs_filter_t){__identity, NULL};
 	}
 
-	{
-		typedef echs_stream_t(*make_stream_f)(echs_instant_t, ...);
-		echs_mod_f f;
-		const char *const *in = argi->inputs;
-		size_t nin = argi->inputs_num;
+	/* create a stream f(s) with f being THIF and s being S */
+	this = make_echs_stream(from, thif, argi->inputs, argi->inputs_num);
 
-		if ((ex = echs_mod_open("echse")) == NULL) {
-			printf("NOPE\n");
-			goto out;
-		} else if ((f = echs_mod_sym(ex, "make_echs_stream")) == NULL) {
-			printf("no m_e_s()\n");
-			goto clos_out;
-		} else if ((s = ((make_stream_f)f)(from, in, nin)).f == NULL) {
-			printf("no stream\n");
-			goto clos_out;
+	/* the iterator */
+	for (echs_event_t e;
+	     (e = echs_stream_next(this),
+	      !__event_0_p(e) && __event_le_p(e, till));) {
+		if (UNLIKELY(materialise(e) < 0)) {
+			break;
 		}
 	}
 
-	/* iterate! */
-	for (echs_event_t e;
-	     (e = echs_stream_next(s),
-	      !__event_0_p(e) && __event_le_p(e, till));) {
-		if (!__event_0_p(e = echs_filter_next(this, e))) {
-			/* output */
-			materialise(e);
-		}
-	}
-	/* drain */
-	for (echs_event_t e;
-	     (e = echs_filter_drain(this),
-	      !__event_0_p(e) && __event_le_p(e, till));) {
-		materialise(e);
-	}
+	/* get all of them streams in here finished */
+	free_echs_stream(this);
 
 	/* free filter */
-	free_echs_filter(this);
-
-	{
-		typedef void(*free_stream_f)(echs_stream_t);
-		echs_mod_f f;
-
-		if ((f = echs_mod_sym(ex, "free_echs_stream")) == NULL) {
-			printf("no f_e_s()\n");
-			goto clos_out;
-		}
-		((free_stream_f)f)(s);
-	}
-
-clos_out:
-	echs_mod_close(ex);
+	free_echs_filter(thif);
 
 out:
 	echs_parser_free(argi);
