7213fff1b294ac2734e0e793da86ff2cca344967
Sebastian Freundt <freundt@ga-group.nl>
2013-03-01 13:52:13 +0000

    minor, rename echse-filter to echs-filter and kick standalone stuff
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/echs-filter.c
+++ src/echs-filter.c
@@ -1,4 +1,4 @@
-/*** echse-filter.c -- filtering events
+/*** echs-filter.c -- filtering events
  *
  * Copyright (C) 2013 Sebastian Freundt
  *
@@ -64,71 +64,6 @@
 #endif	/* UNUSED */
 
 #define logger(what, how, args...)	fprintf(stderr, how "\n", args)
-
-
-#if defined STANDALONE
-static int
-materialise(echs_event_t e)
-{
-	static char buf[256];
-	char *bp = buf;
-
-	/* BEST has the guy */
-	bp += dt_strf(buf, sizeof(buf), e.when);
-	*bp++ = '\t';
-
-	{
-		size_t e_whaz = strlen(e.what);
-		memcpy(bp, e.what, e_whaz);
-		bp += e_whaz;
-	}
-	*bp++ = '\n';
-	*bp = '\0';
-	if (write(STDOUT_FILENO, buf, bp - buf) < 0) {
-		return -1;
-	}
-	return 0;
-}
-
-static echs_strdef_t
-make_argi_stream(echs_instant_t from, const char *const *in, size_t nin)
-{
-	typedef echs_stream_t(*make_stream_f)(echs_instant_t, ...);
-	echs_stream_t s = {NULL};
-	echs_mod_t m;
-	echs_mod_f f;
-
-	if ((m = echs_mod_open("echse")) == NULL) {
-		printf("NOPE\n");
-	} else if ((f = echs_mod_sym(m, "make_echs_stream")) == NULL) {
-		printf("no m_e_s()\n");
-	} else if ((s = ((make_stream_f)f)(from, in, nin)).f == NULL) {
-		printf("no stream\n");
-	}
-	return (echs_strdef_t){s, m};
-}
-
-static void
-free_argi_stream(echs_strdef_t sd)
-{
-	typedef void(*free_stream_f)(echs_stream_t);
-	echs_mod_f f;
-
-	if (UNLIKELY(sd.m == NULL)) {
-		return;
-	}
-
-	if ((f = echs_mod_sym(sd.m, "free_echs_stream")) == NULL) {
-		printf("no f_e_s()\n");
-		goto clos_out;
-	}
-	((free_stream_f)f)(sd.s);
-
-clos_out:
-	echs_mod_close(sd.m);
-	return;
-}
-#endif	/* STANDALONE */
 
 
 /* myself as filter */
@@ -235,88 +170,4 @@ free_echs_stream(echs_stream_t UNUSED(s))
 	return;
 }
 
-
-#if defined STANDALONE
-#if defined __INTEL_COMPILER
-# pragma warning (disable:593)
-# pragma warning (disable:181)
-#elif defined __GNUC__
-# pragma GCC diagnostic ignored "-Wswitch"
-# pragma GCC diagnostic ignored "-Wswitch-enum"
-#endif /* __INTEL_COMPILER */
-#include "echse-filter-clo.h"
-#include "echse-filter-clo.c"
-#if defined __INTEL_COMPILER
-# pragma warning (default:593)
-# pragma warning (default:181)
-#elif defined __GNUC__
-# pragma GCC diagnostic warning "-Wswitch"
-# pragma GCC diagnostic warning "-Wswitch-enum"
-#endif	/* __INTEL_COMPILER */
-
-int
-main(int argc, char *argv[])
-{
-	/* command line options */
-	struct echs_args_info argi[1];
-	echs_instant_t from;
-	echs_instant_t till;
-	echs_strdef_t argi_sd;
-	echs_filter_t thif;
-	echs_stream_t this;
-	int res = 0;
-
-	if (echs_parser(argc, argv, argi)) {
-		res = 1;
-		goto out;
-	}
-
-	if (argi->from_given) {
-		from = dt_strp(argi->from_arg);
-	} else {
-		from = (echs_instant_t){2000, 1, 1};
-	}
-
-	if (argi->till_given) {
-		till = dt_strp(argi->till_arg);
-	} else {
-		till = (echs_instant_t){2037, 12, 31};
-	}
-
-	if (argi->filter_given) {
-		thif = make_echs_filter(from, argi->filter_arg);
-	} else {
-		thif = (echs_filter_t){__identity, NULL};
-	}
-
-	/* generate the input stream to our filter */
-	argi_sd = make_argi_stream(from, argi->inputs, argi->inputs_num);
-
-	/* create a stream f(s) with f being THIF and s being S */
-	this = make_echs_stream(from, thif, argi_sd.s);
-
-	/* the iterator */
-	for (echs_event_t e;
-	     (e = echs_stream_next(this),
-	      !__event_0_p(e) && __event_le_p(e, till));) {
-		if (UNLIKELY(materialise(e) < 0)) {
-			break;
-		}
-	}
-
-	/* get all of them streams in here finished */
-	free_echs_stream(this);
-
-	/* free the argi stream */
-	free_argi_stream(argi_sd);
-
-	/* free filter */
-	free_echs_filter(thif);
-
-out:
-	echs_parser_free(argi);
-	return res;
-}
-#endif	/* STANDALONE */
-
-/* echse-filter.c ends here */
+/* echs-filter.c ends here */
