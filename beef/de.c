1dc3c2e9a1690dfeee97b281a0459690f6f9c4df
Sebastian Freundt <freundt@ga-group.nl>
2013-05-02 15:58:07 +0000

    hygiene, instead of opening the easter DSO twice, use clone_echs_stream() primitive in de holiday list
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- beef/de.c
+++ beef/de.c
@@ -1,6 +1,7 @@
 #include <echse.h>
 #include <strdef.h>
 #include <builders.h>
+#include <strctl.h>
 
 #if !defined countof
 # define countof(x)		(sizeof(x) / sizeof(*x))
@@ -11,27 +12,26 @@ echs_stream_t oct3;
 echs_stream_t may1;
 echs_stream_t xmas;
 echs_stream_t boxd;
-echs_stream_t easter_sel;
-echs_stream_t ascension_sel;
-echs_strdef_t east1;
-echs_strdef_t east2;
+echs_stream_t easter;
+echs_stream_t ascension;
+echs_strdef_t g_east;
 
 
 echs_stream_t
 make_echs_stream(echs_instant_t i, ...)
 {
+	echs_strdef_t east = echs_open_stream(i, "easter");
 	echs_stream_t all[] = {
 		newy = echs_every_year(i, JAN, 1),
 		may1 = echs_every_year(i, MAY, 1),
 		oct3 = echs_every_year(i, OCT, 3),
 		xmas = echs_every_year(i, DEC, 25),
 		boxd = echs_every_year(i, DEC, 26),
-		easter_sel = echs_select(
-			(east1 = echs_open_stream(i, "easter")).s, "EASTER"),
-		ascension_sel = echs_select(
-			(east2 = echs_open_stream(i, "easter")).s, "ASCENSION"),
+		easter = echs_select(clone_echs_stream(east.s), "EASTER"),
+		ascension = echs_select(clone_echs_stream(east.s), "ASCENSION"),
 	};
 
+	g_east = east;
 	echs_every_set_state(newy, "Neujahr");
 	echs_every_set_state(may1, "Tag_der_Arbeit");
 	echs_every_set_state(oct3, "Tag_der_Einheit");
@@ -49,10 +49,9 @@ free_echs_stream(echs_stream_t s)
 	echs_free_every(oct3);
 	echs_free_every(xmas);
 	echs_free_every(boxd);
-	echs_free_select(easter_sel);
-	echs_free_select(ascension_sel);
-	echs_close_stream(east1);
-	echs_close_stream(east2);
+	unclone_echs_stream(echs_free_select(easter));
+	unclone_echs_stream(echs_free_select(ascension));
+	echs_close_stream(g_east);
 	return;
 }
 
