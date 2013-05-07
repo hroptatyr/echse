ac8332ebccc9a2b8254afe03a9b187939675e5b2
Sebastian Freundt <freundt@ga-group.nl>
2013-05-07 15:20:50 +0000

    minor, adapt uk holiday list, using echs_move() to move christmas and boxing day
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- beef/uk.c
+++ beef/uk.c
@@ -10,34 +10,40 @@ echs_stream_t newy;
 echs_stream_t mayd;
 echs_stream_t spring;
 echs_stream_t summer;
+echs_stream_t easter;
+echs_stream_t xbox;
 echs_stream_t xmas;
 echs_stream_t boxd;
-echs_stream_t easter;
 echs_strdef_t g_east;
+echs_strdef_t g_weekend;
 
 
 echs_stream_t
 make_echs_stream(echs_instant_t i, ...)
 {
 	echs_strdef_t east = echs_open_stream(i, "easter");
-	echs_stream_t all[] = {
-		newy = echs_every_year(i, JAN, 1U),
-		mayd = echs_every_year(i, MAY, FIRST(MON)),
-		spring = echs_every_year(i, MAY, LAST(MON)),
-		summer = echs_every_year(i, AUG, LAST(MON)),
-		xmas = echs_every_year(i, DEC, 25),
-		boxd = echs_every_year(i, DEC, 26),
-		easter = ECHS_SELECT(east.s, {"GOODFRI", "EASTER", "EASTERMON"}),
-	};
+	echs_strdef_t weekend = echs_open_stream(i, "weekend");
 
-	g_east = east;
-	echs_every_set_state(mayd, "Mayday");
+	newy = echs_every_year(i, JAN, 1U);
+	mayd = echs_every_year(i, MAY, FIRST(MON));
+	spring = echs_every_year(i, MAY, LAST(MON));
+	summer = echs_every_year(i, AUG, LAST(MON));
+	easter = ECHS_SELECT(east.s, {"GOODFRI", "EASTER", "EASTERMON"});
+
+	xmas = echs_every_year(i, DEC, 25);
+	boxd = echs_every_year(i, DEC, 26);
 	echs_every_set_state(xmas, "Christmas");
 	echs_every_set_state(boxd, "Boxing_Day");
+	xbox = echs_move_after(weekend.s, ECHS_MUX(xmas, boxd));
+
+	g_east = east;
+	g_weekend = weekend;
+	echs_every_set_state(mayd, "Mayday");
 	echs_every_set_state(newy, "New_Year");
 	echs_every_set_state(summer, "Summer_Bank_Holiday");
 	echs_every_set_state(spring, "Spring_Bank_Holiday");
-	return echs_mux(countof(all), all);
+
+	return ECHS_MUX(newy, mayd, spring, summer, easter, xbox);
 }
 
 void
@@ -46,12 +52,17 @@ free_echs_stream(echs_stream_t s)
 	echs_free_mux(s);
 	echs_free_every(newy);
 	echs_free_every(mayd);
-	echs_free_every(xmas);
-	echs_free_every(boxd);
 	echs_free_every(summer);
 	echs_free_every(spring);
 	echs_free_select(easter);
+	echs_free_move(xbox);
+
+	/* free tentative resources */
+	echs_free_every(xmas);
+	echs_free_every(boxd);
+
 	echs_close_stream(g_east);
+	echs_close_stream(g_weekend);
 	return;
 }
 
