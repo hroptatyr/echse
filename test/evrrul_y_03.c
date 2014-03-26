4772f7e158a24106f9c8d1325837f98f48e63a10
Sebastian Freundt <hroptatyr@fresse.org>
2014-03-26 23:20:09 +0000

    test, provide some simple evrrul (C-based) regressions
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

---
+++ test/evrrul_y_03.c
@@ -0,0 +1,79 @@
+/*** evrrul_y.c -- testing the yearly rrule
+ *
+ * Copyright (C) 2013-2014 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of echse.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "echse.h"
+/* specifically testing the evrrul routines */
+#include "evrrul.h"
+
+
+int
+main(int argc, char *argv[])
+{
+	echs_event_t proto = {
+		.from = {.y = 2006, .m = 1, .d = 1, .H = ECHS_ALL_DAY},
+		.till = {.y = 2006, .m = 1, .d = 1, .H = ECHS_ALL_DAY},
+		.uid = intern("Payday", 6U),
+	};
+	evrrul_param_t p = {
+		.proto = proto,
+		.till = echs_max_instant(),
+		.count = 10U,
+	};
+	echs_evstrm_t y = echs_yearly(p, JUN, LAST(FRI));
+	unsigned int n = 0U;
+
+	for (echs_event_t e;
+	     !echs_event_0_p(e = echs_evstrm_next(y)); n++) {
+		char buf[32U];
+
+		dt_strf(buf, sizeof(buf), e.from);
+		printf("VEVENT %u: %s\n", n, buf);
+	}
+	free_echs_evstrm(y);
+
+	clear_interns();
+	clear_bufpool();
+	return 0;
+}
+
+/* evrrul_y.c ends here */
