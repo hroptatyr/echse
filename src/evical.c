dc1e6ca092bd1bae3cb8e23eebb91a50bdbfc97b
Sebastian Freundt <hroptatyr@fresse.org>
2014-03-30 22:53:43 +0000

    minor, provide rrule snarfer
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

--- src/evical.c
+++ src/evical.c
@@ -39,20 +39,46 @@
 #endif	/* HAVE_CONFIG_H */
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 #include "evical.h"
 #include "intern.h"
 #include "bufpool.h"
+#include "bitint.h"
 #include "dt-strpf.h"
+#include "evrrul.h"
 #include "nifty.h"
 #include "evical-gp.c"
+#include "evrrul-gp.c"
 
 typedef struct evarr_s *evarr_t;
 
 struct evarr_s {
 	size_t nev;
 	echs_event_t ev[];
+};
+
+struct rrulsp_s {
+	echs_freq_t freq;
+	unsigned int count;
+	unsigned int inter;
+
+	bitint31_t dom;
+	bitint383_t doy;
+
+	/* we'll store mon->1, tue->2, ..., 1mon->8, 2mon->15, ...
+	 * -1mon->-1, -1tue->-2, ..., -2mon->-8, -3mon->-15, ... */
+	bitint383_t dow;
+
+	bituint31_t mon;
+	bitint63_t wk;
+
+	bituint31_t H;
+	bituint63_t M, S;
+
+	bitint383_t pos;
+	bitint383_t easter;
 };
 
 
@@ -66,6 +92,183 @@ snarf_value(const char *s)
 		res = dt_strp(sp + 1);
 	}
 	return res;
+}
+
+static echs_freq_t
+snarf_freq(const char *spec)
+{
+	switch (*spec) {
+	case 'Y':
+		return FREQ_YEARLY;
+	case 'M':
+		if (UNLIKELY(spec[1] == 'I')) {
+			return FREQ_MINUTELY;
+		}
+		return FREQ_MONTHLY;
+	case 'W':
+		return FREQ_WEEKLY;
+	case 'D':
+		return FREQ_DAILY;
+	case 'H':
+		return FREQ_HOURLY;
+	case 'S':
+		return FREQ_SECONDLY;
+	default:
+		break;
+	}
+	return FREQ_NONE;
+}
+
+static echs_wday_t
+snarf_wday(const char *s)
+{
+	switch (*s) {
+	case 'M':
+		return MON;
+	case 'W':
+		return WED;
+	case 'F':
+		return FRI;
+
+	case 'R':
+		return THU;
+	case 'A':
+		return SAT;
+
+	case 'T':
+		if (s[1U] == 'H') {
+			return THU;
+		}
+		return TUE;
+	case 'S':
+		if (s[1U] == 'A') {
+			return SAT;
+		}
+		return SUN;
+	default:
+		break;
+	}
+	return MIR;
+}
+
+static void
+snarf_rrule(const char *s, size_t z)
+{
+	struct rrulsp_s rr = {FREQ_NONE};
+
+	for (const char *sp = s, *const ep = s + z, *eofld;
+	     sp < ep; sp = eofld + 1) {
+		const struct rrul_key_cell_s *c;
+		const char *kv;
+		size_t kz;
+
+		if (UNLIKELY((eofld = strchr(sp, ';')) == NULL)) {
+			eofld = ep;
+		}
+		/* find the key-val separator (=) */
+		if (UNLIKELY((kv = strchr(sp, '=')) == NULL)) {
+			kz = eofld - sp;
+		} else {
+			kz = kv - sp;
+		}
+		/* try a lookup */
+		if (UNLIKELY((c = __evrrul_key(sp, kz)) == NULL)) {
+			/* not found */
+			continue;
+		}
+		/* otherwise do a bit of inspection */
+		switch (c->key) {
+			long int tmp;
+			char *on;
+		case KEY_FREQ:
+			/* read the spec as well */
+			rr.freq = snarf_freq(++kv);
+			break;
+
+		case KEY_COUNT:
+		case KEY_INTER:
+			tmp = atol(++kv);
+			switch (c->key) {
+			case KEY_COUNT:
+				rr.count = (unsigned int)tmp;
+				break;
+			case KEY_INTER:
+				rr.inter = (unsigned int)tmp;
+				break;
+			}
+			break;
+
+		case BY_WDAY:
+			/* this one's special in that weekday names
+			 * are allowed to follow the indicator number */
+			do {
+				echs_wday_t w;
+
+				tmp = strtol(++kv, &on, 10);
+				if ((w = snarf_wday(on)) == MIR) {
+					continue;
+				}
+				/* otherwise assign */
+				ass_bi383(&rr.dow, (tmp *= 7, tmp += w, tmp));
+			} while ((kv = strchr(on, ',')) != NULL);
+			break;
+		case BY_MON:
+		case BY_HOUR:
+		case BY_MIN:
+		case BY_SEC:
+			do {
+				tmp = strtoul(++kv, &on, 10);
+				switch (c->key) {
+				case BY_MON:
+					rr.mon = ass_bui31(rr.mon, tmp);
+					break;
+				case BY_HOUR:
+					rr.H = ass_bui31(rr.H, tmp);
+					break;
+				case BY_MIN:
+					rr.M = ass_bui63(rr.M, tmp);
+					break;
+				case BY_SEC:
+					rr.S = ass_bui63(rr.S, tmp);
+					break;
+				}
+			} while (*(kv = on) == ',');
+			break;
+
+		case BY_MDAY:
+		case BY_WEEK:
+		case BY_YDAY:
+		case BY_POS:
+		case BY_EASTER:
+			/* these ones take +/- values */
+			do {
+				tmp = strtol(++kv, &on, 10);
+				switch (c->key) {
+				case BY_MDAY:
+					rr.dom = ass_bi31(rr.dom, tmp - 1);
+					break;
+				case BY_WEEK:
+					rr.wk = ass_bi63(rr.wk, tmp);
+					break;
+				case BY_YDAY:
+					ass_bi383(&rr.doy, tmp);
+					break;
+				case BY_POS:
+					ass_bi383(&rr.pos, tmp);
+					break;
+				case BY_EASTER:
+					ass_bi383(&rr.easter, tmp);
+					break;
+				}
+			} while (*(kv = on) == ',');
+			break;
+
+		default:
+		case KEY_UNK:
+			break;
+		}
+	}
+	return;
 }
 
 static void
@@ -98,6 +301,13 @@ snarf_fld(echs_event_t ev[static 1U], const char *line, size_t llen)
 		break;
 	case FLD_DTEND:
 		ev->till = snarf_value(lp);
+		break;
+	case FLD_RRULE:
+		if (UNLIKELY(*lp++ != ':' && (lp = strchr(lp, ':')) == NULL)) {
+			break;
+		}
+		/* otherwise snarf him */
+		snarf_rrule(lp, ep - lp);
 		break;
 	case FLD_SUMM:
 		if (UNLIKELY(*lp++ != ':' && (lp = strchr(lp, ':')) == NULL)) {
@@ -271,7 +481,7 @@ next_evical_vevent(echs_evstrm_t s)
 	return this->ea->ev[this->i++];
 }
 
-
+
 #define T	echs_event_t
 
 static inline __attribute__((pure)) bool
@@ -293,6 +503,65 @@ make_echs_evical(const char *fn)
 	/* got it, sort it then */
 	WikiSort(a->ev, a->nev);
 	return make_evical_vevent(a);
+}
+
+
+struct evrrul_s {
+	echs_evstrm_class_t class;
+	/* the actual recurrence rule */
+	struct rrulsp_s rr;
+};
+
+static echs_event_t next_evrrul(echs_evstrm_t);
+static void free_evrrul(echs_evstrm_t);
+static echs_evstrm_t clone_evrrul(echs_evstrm_t);
+
+static const struct echs_evstrm_class_s evrrul_cls = {
+	.next = next_evrrul,
+	.free = free_evrrul,
+	.clone = clone_evrrul,
+};
+
+static echs_evstrm_t
+make_evrrul(const struct rrulsp_s *rr)
+{
+	struct evrrul_s *res = malloc(sizeof(*res));
+
+	res->class = &evrrul_cls;
+	res->rr = *rr;
+	return (echs_evstrm_t)res;
+}
+
+static void
+free_evrrul(echs_evstrm_t s)
+{
+	struct evrrul_s *this = (struct evrrul_s*)s;
+
+	free(this);
+	return;
+}
+
+static echs_evstrm_t
+clone_evrrul(echs_evstrm_t s)
+{
+	struct evrrul_s *this = (struct evrrul_s*)s;
+
+	return make_evrrul(&this->rr);
+}
+
+static echs_event_t
+next_evrrul(echs_evstrm_t s)
+{
+	struct evrrul_s *this = (struct evrrul_s*)s;
+
+	return nul;
+}
+
+
+echs_evstrm_t
+make_echs_evrrul(const char *fn)
+{
+	return NULL;
 }
 
 /* evical.c ends here */
