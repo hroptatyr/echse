9e94dc52ddbe4f5fd99b23791f69c7f7f8aa5c70
Sebastian Freundt <freundt@ga-group.nl>
2015-07-15 09:14:17 +0000

    minor, reply to GET bla.ics requests with a tentative 204

--- src/echsd.c
+++ src/echsd.c
@@ -112,6 +112,63 @@ xstrlcpy(char *restrict dst, const char *src, size_t dsz)
 	return ssz;
 }
 
+static char*
+xmemmem(const char *hay, const size_t hayz, const char *ndl, const size_t ndlz)
+{
+	const char *const eoh = hay + hayz;
+	const char *const eon = ndl + ndlz;
+	const char *hp;
+	const char *np;
+	const char *cand;
+	unsigned int hsum;
+	unsigned int nsum;
+	unsigned int eqp;
+
+	/* trivial checks first
+         * a 0-sized needle is defined to be found anywhere in haystack
+         * then run strchr() to find a candidate in HAYSTACK (i.e. a portion
+         * that happens to begin with *NEEDLE) */
+	if (ndlz == 0UL) {
+		return deconst(hay);
+	} else if ((hay = memchr(hay, *ndl, hayz)) == NULL) {
+		/* trivial */
+		return NULL;
+	}
+
+	/* First characters of haystack and needle are the same now. Both are
+	 * guaranteed to be at least one character long.  Now computes the sum
+	 * of characters values of needle together with the sum of the first
+	 * needle_len characters of haystack. */
+	for (hp = hay + 1U, np = ndl + 1U, hsum = *hay, nsum = *hay, eqp = 1U;
+	     hp < eoh && np < eon;
+	     hsum ^= *hp, nsum ^= *np, eqp &= *hp == *np, hp++, np++);
+
+	/* HP now references the (NZ + 1)-th character. */
+	if (np < eon) {
+		/* haystack is smaller than needle, :O */
+		return NULL;
+	} else if (eqp) {
+		/* found a match */
+		return deconst(hay);
+	}
+
+	/* now loop through the rest of haystack,
+	 * updating the sum iteratively */
+	for (cand = hay; hp < eoh; hp++) {
+		hsum ^= *cand++;
+		hsum ^= *hp;
+
+		/* Since the sum of the characters is already known to be
+		 * equal at that point, it is enough to check just NZ - 1
+		 * characters for equality,
+		 * also CAND is by design < HP, so no need for range checks */
+		if (hsum == nsum && memcmp(cand, ndl, ndlz - 1U) == 0) {
+			return deconst(cand);
+		}
+	}
+	return NULL;
+}
+
 static void
 block_sigs(void)
 {
@@ -695,7 +752,13 @@ sock_data_cb(EV_P_ ev_io *w, int UNUSED(revents))
 	ssize_t nrd;
 
 	nrd = read(fd, buf, sizeof(buf));
-	fwrite(buf, 1, nrd, stdout);
+	/* check the command we're supposed to obey */
+	if (xmemmem(buf, nrd, " HTTP/1.1\r\n", strlenof(" HTTP/1.1\r\n"))) {
+		/* aaaah http shit */
+		static const char rpl[] = "\
+HTTP/1.1 204 No Content\r\n\r\n";
+		write(fd, rpl, strlenof(rpl));
+	}
 
 	ev_io_stop(EV_A_ w);
 	shut_conn((struct echs_conn_s*)w);
