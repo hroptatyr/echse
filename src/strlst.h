74773b40e76460c346513d8d1ae2548605ca211a
Sebastian Freundt <freundt@ga-group.nl>
2015-07-21 11:10:38 +0000

    minor, provide strlst implementation to aid building and using env-like string lists

---
+++ src/strlst.h
@@ -0,0 +1,68 @@
+/*** strlst.h -- list of NULL terminated strings a la env
+ *
+ * Copyright (C) 2013-2015 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of echse.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if !defined INCLUDED_strlst_h_
+#define INCLUDED_strlst_h_
+
+struct strlst_s {
+	/* pool for the strings */
+	char *s;
+	/* index of next string */
+	size_t i;
+	/* index of next pointer to string */
+	size_t nl;
+	/* the actual char**, always NULL-terminated */
+	char *l[];
+};
+
+
+/**
+ * Clone SL deeply. */
+extern struct strlst_s *clone_strlst(const struct strlst_s *sl);
+
+/**
+ * Add a string S to L. */
+extern void strlst_add(struct strlst_s *l[static 1U], const char *s);
+
+/**
+ * Add a string S of length N to L. */
+extern void strlst_addn(struct strlst_s *l[static 1U], const char *s, size_t n);
+
+/**
+ * String list dtor. */
+extern void free_strlst(struct strlst_s *l);
+
+#endif	/* INCLUDED_strlst_h_ */
