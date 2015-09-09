c0b01d91d3ae30847a4833e386a0070726291b5c
Sebastian Freundt <freundt@ga-group.nl>
2015-09-09 14:54:51 +0000

    hygiene, rewrite xstrlcpy() in terms of xstrlncpy()

--- src/echsq.c
+++ src/echsq.c
@@ -87,16 +87,21 @@ serror(const char *fmt, ...)
 	return;
 }
 
-static size_t
-xstrlcpy(char *restrict dst, const char *src, size_t dsz)
+static inline size_t
+xstrlncpy(char *restrict dst, size_t dsz, const char *src, size_t ssz)
 {
-	size_t ssz = strlen(src);
-	if (ssz > dsz) {
+	if (UNLIKELY(ssz > dsz)) {
 		ssz = dsz - 1U;
 	}
 	memcpy(dst, src, ssz);
 	dst[ssz] = '\0';
 	return ssz;
+}
+
+static size_t
+xstrlcpy(char *restrict dst, const char *src, size_t dsz)
+{
+	return xstrlncpy(dst, dsz, src, strlen(src));
 }
 
 
