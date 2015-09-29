e7a33650086646a5a903509fa57346d6284ee4fc
Sebastian Freundt <freundt@ga-group.nl>
2015-09-29 06:40:59 +0000

    chore, switch to xxhash (and bury murmur)

--- src/tzob.c
+++ src/tzob.c
@@ -45,10 +45,8 @@
 #include <sys/time.h>
 #include "tzob.h"
 #include "tzraw.h"
+#include "hash.h"
 #include "nifty.h"
-
-/* a hash is the bucket locator and a chksum for collision detection */
-typedef uint_fast32_t hash_t;
 
 /* the beef table */
 static struct {
@@ -78,76 +76,6 @@ static struct {
 	size_t cnt;
 } tmfu[16U];
 static zif_t zmfu[16U];
-
-static hash_t
-murmur(const uint8_t *str, size_t len)
-{
-/* See http://murmurhash.googlepages.com/ for info about the Murmur hash.
- * Murmur is a non-cryptographic hashing hash  is by Austin Appleby. */
-	const uint_fast32_t c1 = 0xcc9e2d51U;
-	const uint_fast32_t c2 = 0x1b873593U;
-	const size_t nb = len / 4U;
-	const size_t nr = len % 4U;
-	const uint8_t *const tail = (const uint8_t*)(str + nb * 4U);
-#if BYTE_ORDER == LITTLE_ENDIAN
-	const uint32_t *b = (const uint32_t*)tail;
-#endif	/* LITTLE_ENDIAN */
-	hash_t h = 0U;
-	hash_t k;
-
-	for (ssize_t i = -nb; i; i++) {
-#if BYTE_ORDER == LITTLE_ENDIAN
-		k = b[i];
-#elif BYTE_ORDER == BIG_ENDIAN
-		k = 0U;
-		k ^= tail[4 * i + 0] << 0U;
-		k ^= tail[4 * i + 1] << 8U;
-		k ^= tail[4 * i + 2] << 16U;
-		k ^= tail[4 * i + 3] << 24U;
-#else
-# warning byte order detection failed, expect bogosity
-#endif	/* BYTE_ORDERS */
-		k *= c1;
-		k &= 0xffffffffU;
-		k = (k << 15U) ^ (k >> (32U - 15U));
-		k *= c2;
-		h ^= k;
-		h &= 0xffffffffU;
-		h = (h << 13U) ^ (h >> (32U - 13U));
-		h = (h * 5U) + 0xe6546b64U;
-	}
-	/* reset k and process the tail */
-	k = 0U;
-	switch (nr) {
-	case 3U:
-		k ^= tail[2U] << 16U;
-		/*@fallthrough@*/
-	case 2U:
-		k ^= tail[1U] << 8U;
-		/*@fallthrough@*/
-	case 1U:
-		k ^= tail[0U] << 0U;
-		k *= c1;
-		k &= 0xffffffffU;
-		k = (k << 15U) | (k >> (32U - 15U));
-		k *= c2;
-		h ^= k;
-		/*@fallthrough@*/
-	case 0U:
-		break;
-	}
-
-	h ^= nb;
-	h &= 0xffffffffU;
-	h ^= h >> 16U;
-	h *= 0x85ebca6bU;
-	h &= 0xffffffffU;
-	h ^= h >> 13U;
-	h *= 0xc2b2ae35U;
-	h &= 0xffffffffU;
-	h ^= h >> 16U;
-	return h;
-}
 
 static void*
 recalloc(void *buf, size_t nmemb_ol, size_t nmemb_nu, size_t membz)
@@ -324,7 +252,7 @@ echs_tzob(const char *str, size_t len)
 	 * the first stack is 256 entries wide, the next stack is 1024
 	 * bytes wide, but only hosts 768 entries because the probe is
 	 * constructed so that the lowest 8bits are always 0. */
-	const hash_t hx = murmur((const uint8_t*)str, len);
+	const hash_t hx = hash(str, len);
 	hash_t k = hx;
 
 	/* just try what we've got */
