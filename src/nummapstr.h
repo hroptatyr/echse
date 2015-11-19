6817028b4fb7a4e8160ebebb9b0c82b3bba20e3e
Sebastian Freundt <freundt@ga-group.nl>
2015-11-19 15:46:54 +0000

    Revert "chore, tentatively turn nummapstr_t into struct"
    This reverts commit 87e555b06f15e9ad6c422895b2506a74c73881d2.

--- src/nummapstr.h
+++ src/nummapstr.h
@@ -39,44 +39,40 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct {
-	uintptr_t ns;
-} nummapstr_t;
-#define NUMMAPSTR_NAN	((uintptr_t)-1)
+typedef uintptr_t nummapstr_t;
+#define NUMMAPSTR_NAN	((nummapstr_t)-1)
 #define NUMMAPSTR_WID	(sizeof(nummapstr_t) * 8U)
-#define NUMMAPSTR_MSK	((uintptr_t)1 << (NUMMAPSTR_WID - 1U))
-#define NUMMAPSTR_MIN	((nummapstr_t){0})
-#define NUMMAPSTR_MAX	((nummapstr_t){INTPTRMAX})
-
-_Static_assert(sizeof(nummapstr_t) == sizeof(uintptr_t), "Uh oh");
+#define NUMMAPSTR_MSK	((nummapstr_t)1 << (NUMMAPSTR_WID - 1U))
+#define NUMMAPSTR_MIN	((nummapstr_t)0)
+#define NUMMAPSTR_MAX	((nummapstr_t)INTPTRMAX)
 
 
 static inline char*
 nummapstr_str(nummapstr_t x)
 {
 /* Return the string in X as char*, or NULL if X is a number. */
-	return x.ns & NUMMAPSTR_MSK ? NULL : (void*)x.ns;
+	return x & NUMMAPSTR_MSK ? NULL : (void*)x;
 }
 
 static inline uintptr_t
 nummapstr_num(nummapstr_t x)
 {
 /* Return the number in X as uintptr_t, or NUMMAPSTR_NAN if X is a string. */
-	return x.ns & NUMMAPSTR_MSK ? x.ns ^ NUMMAPSTR_MSK : NUMMAPSTR_NAN;
+	return x & NUMMAPSTR_MSK ? x ^ NUMMAPSTR_MSK : NUMMAPSTR_NAN;
 }
 
 static inline nummapstr_t
 nummapstr_bang_num(uintptr_t n)
 {
 /* Return N as nummapstr_t */
-	return (nummapstr_t){n | NUMMAPSTR_MSK};
+	return n | NUMMAPSTR_MSK;
 }
 
 static inline nummapstr_t
 nummapstr_bang_str(const char *s)
 {
 /* Return S as nummapstr_t */
-	return (nummapstr_t){(uintptr_t)s};
+	return (uintptr_t)s;
 }
 
 #endif	/* INCLUDED_nummapstr_h_ */
