f7fad53826f043ea2c7aa9949cfe03079c9f720b
Sebastian Freundt <freundt@ga-group.nl>
2020-07-15 09:14:00 +0000

    fix, heed advice not to use %llx with long unsigneds

--- test/bitint_test_07.c
+++ test/bitint_test_07.c
@@ -11,20 +11,20 @@ main(int argc, char *argv[])
 	bitint63_t x = {0U};
 
 	x = ass_bi63(x, 36);
-	printf("%llx %llx\n", x.pos, x.neg);
+	printf("%lx %lx\n", x.pos, x.neg);
 
 	x = ass_bi63(x, 40);
-	printf("%llx %llx\n", x.pos, x.neg);
+	printf("%lx %lx\n", x.pos, x.neg);
 
 	x = ass_bi63(x, -4);
-	printf("%llx %llx\n", x.pos, x.neg);
+	printf("%lx %lx\n", x.pos, x.neg);
 
 	x = ass_bi63(x, 0);
-	printf("%llx %llx\n", x.pos, x.neg);
+	printf("%lx %lx\n", x.pos, x.neg);
 	x = ass_bi63(x, 1);
-	printf("%llx %llx\n", x.pos, x.neg);
+	printf("%lx %lx\n", x.pos, x.neg);
 	x = ass_bi63(x, -33);
-	printf("%llx %llx\n", x.pos, x.neg);
+	printf("%lx %lx\n", x.pos, x.neg);
 	return 0;
 }
 
